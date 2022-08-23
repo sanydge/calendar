814893b2cb99181aa22ae9c845931bc2e1f94341
sanydge <sanydge@gmail.com>
2022-08-23 09:23:47 +0300

    JS on schedule

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const date = new Date();
 
-const renderCalendar=()=>{
+const renderCalendar = () => {
+    const year = `${date.getFullYear()}`;
+    const month = `${date.getMonth() + 1}`.padStart(2, "0");
     const monthDays = document.querySelector('.days')
-    const months = ["January", "February", "March", "April", "May","June", "July", "August", "September", "October", "November", "December"];
-    const lastDay = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const lastDay = new Date(parseInt(year), parseInt(month), 0).getDate();
     const previousLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
     const firstDayIndex = date.getDay() - 2;
-    const lastDayIndex = new Date(date.getFullYear(), date.getMonth()+1, 0).getDay();
+    const lastDayIndex = new Date(lastDay).getDay();
     const nextDays = 7 - lastDayIndex - 1;
 
 
@@ -14,25 +16,37 @@ const renderCalendar=()=>{
     document.querySelector(".date p").innerHTML = date.getFullYear();
     document.querySelector(".schedule p").innerHTML = new Date().toDateString();
 
-    let days="";
 
-    for(let b=firstDayIndex; b>0; b--){
-        days+=`<div class="previous-date">${previousLastDay - b + 1}</div>`
+    let days = "";
+    for (let b = firstDayIndex; b > 0; b--) {
+        days += `<div class="previous-date">${previousLastDay - b + 1}</div>`
     }
 
-    for(let a=1; a<=lastDay; a++){
-        if(a===new Date().getDate()&&date.getMonth()===new Date().getMonth()){
-            days+=`<div class="today">${a}</div>`;
-        }else{
-            days+=`<div>${a}</div>`;
-        }
-
+    for (let a = 1; a <= lastDay; a++) {
+        const isToday = a === new Date().getDate() && date.getMonth() === new Date().getMonth();
+        const day = `${a}`.padStart(2, "0");
+        days += `<div class="${isToday ? 'today' : ''}" data-date="${year}-${month}-${day}">${a}</div>`;
     }
 
-    for(let c=1; c<=nextDays;c++){
-        days+=`<div class="next-date">${c}</div>`;
+    for (let c = 1; c <= nextDays; c++) {
+        days += `<div class="next-date">${c}</div>`;
     }
+
     monthDays.innerHTML = days;
+
+    let daysList = document.querySelectorAll(".days [data-date]");
+
+
+    daysList.forEach(function (elem){
+        elem.addEventListener("click", function (e){
+           console.log(elem.dataset.date);
+            document.querySelector(".schedule p").innerHTML = new Date(elem.dataset.date).toDateString();
+        })
+    });
+
+
+    console.log(daysList);
+
 
 }
 
